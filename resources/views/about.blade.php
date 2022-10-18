14869ba36984ffec2b22161b6bdeeae6f8e74c10
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2022-10-19 00:33:47 +0700

    update blog
    update blog

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -11,62 +11,65 @@
 @endsection
 
 @section('content')
-<div class="mt-4 container">
-    <div class="d-flex justify-content-center">
-        <div class="col-md-5 d-flex justify-content-center">
-            <img src="img/portofolio1.png" class="myPhoto w-75" alt="">
-        </div>
-        <div class="col-md-7 d-flex align-items-center">
-            <div>
-                <h3 class="mb-1 name text-danger">Bangkit Juang Raharjo</h3>
-                <h1 class="mt-1 mb-1 profession text-light">Backend Developer</h1>
-                <p class="mt-1 mb-5 text-light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique
-                    provident blanditiis magnam non numquam, excepturi veniam quis omnis accusamus ex consequatur
-                    molestiae voluptates, aperiam, tenetur cupiditate molestias aliquam natus. Necessitatibus aperiam,
-                    molestias atque eum, ullam, eos repellat consectetur dolorum nesciunt culpa veritatis cupiditate. Ad
-                    veniam iste facilis cumque perferendis magni!</p>
-                <a href="#" class="btn btn-danger mt-5">ABOUT ME!</a>
+<div class="mt-4 container bg-primary">
+    <div class="description">
+        <img src="img/portofolio1.png" class="my-photo">
+        <div class="about-me w-75 h-auto d-flex align-items-center">
+            <div class="bg-dark">
+                <h3 class="name text-danger">
+                    Bangkit Juang Raharjo
+                </h3>
+                <h1 class="text-white" style="font-family: Montserrat;">Backend Developer</h1>
+                <p class="mt-5 text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab beatae fugiat qui
+                    modi praesentium possimus impedit, deleniti quisquam consectetur totam vel voluptate tenetur illo
+                    porro ratione expedita magnam inventore est rerum assumenda ipsa consequuntur placeat ducimus? Odio,
+                    tempora. Praesentium magni earum dolores dicta culpa voluptatem ex corporis quas explicabo eveniet
+                    error blanditiis quae nulla voluptatum laboriosam corrupti recusandae labore aliquam, impedit,
+                    architecto voluptates! Architecto impedit obcaecati illum, aliquid nobis error.</p>
+                <a href="#" class="mt-5 btn btn-danger">About Me!</a>
             </div>
         </div>
     </div>
-
-    <div class="mt-5">
-        <div class="contoh card-place">
-
-            <div class="grid">
-                <div class="card g-col-12" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                            content.</p>
-                            <a href="#" class="card-link">Card link</a>
-                            <a href="#" class="card-link">Another link</a>
-                    </div>
-                </div>
-                <div class="card" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                            content.</p>
-                            <a href="#" class="card-link">Card link</a>
-                            <a href="#" class="card-link">Another link</a>
-                    </div>
-                </div>
-                <div class="card" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                            content.</p>
-                            <a href="#" class="card-link">Card link</a>
-                            <a href="#" class="card-link">Another link</a>
-                    </div>
+    <div class="portofolio mt-5 d-flex justify-content-evenly">
+        <div class="card bg-dark text-white" style="width: 18rem; height: 19rem;">
+            <div class="card-body d-flex align-items-center">
+                <div>
+                    <i class="bi bi-code-slash code"></i>
+                    <h5 class="card-title"></h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's
+                        content.</p>
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
                 </div>
             </div>
         </div>
-</div>
-
+        <div class="card bg-dark text-white" style="width: 18rem; height: 19rem;">
+            <div class="card-body d-flex align-items-center">
+                <div>
+                    <h5 class="card-title">Card title</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's
+                        content.</p>
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                </div>
+            </div>
+        </div>
+        <div class="card bg-dark text-white" style="width: 18rem; height: 19rem;">
+            <div class="card-body d-flex align-items-center">
+                <div>
+                    <h5 class="card-title">Card title</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's
+                        content.</p>
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
